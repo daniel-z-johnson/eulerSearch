acd9f53379401be726faef3e7a8adfa22260a7c6
prime23 <danzaljohnson@gmail.com>
2017-01-06 22:45:45 -0700

    Initial Commit

---
+++ src/main/java/com/self/exercise/search/euler/EulerApplication.java
@@ -0,0 +1,12 @@
+package com.self.exercise.search.euler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EulerApplication.class, args);
+	}
+}
