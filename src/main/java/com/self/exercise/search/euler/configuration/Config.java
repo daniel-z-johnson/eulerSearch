1dd9a6b69eef74e00655957f92d4c5ccf90a719a
prime23 <danzaljohnson@gmail.com>
2017-01-13 23:51:52 -0700

    Add config class, added dependencies, added bean TransportClient

---
+++ src/main/java/com/self/exercise/search/euler/configuration/Config.java
@@ -0,0 +1,29 @@
+package com.self.exercise.search.euler.configuration;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by daniel on 1/13/17.
+ */
+@Configuration
+public class Config {
+
+    @Bean
+    public TransportClient transportClient(
+            @Value("${es.url}") String host,
+            @Value("${es.port}") int port
+    ) throws UnknownHostException {
+        return new PreBuiltTransportClient(Settings.EMPTY)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(host), port));
+    }
+
+}
